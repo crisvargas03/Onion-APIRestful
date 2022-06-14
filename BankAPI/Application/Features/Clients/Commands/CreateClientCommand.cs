7105b2943ab0367f252f854df5ede5f49ce8e5bc
Cristian Isaac Vargas <65677401+crisvargas03@users.noreply.github.com>
2022-06-13 20:28:23 -0400

    Config: Create the configuration and the validations Behaviors. Also creating the Base Entity and the Client Entity.

---
+++ BankAPI/Application/Features/Clients/Commands/CreateClientCommand.cs
@@ -0,0 +1,22 @@
+﻿using Application.Wrappers;
+using MediatR;
+
+namespace Application.Features.Clients.Commands
+{
+    public class CreateClientCommand : IRequest<Response<int>>
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+    public class CreteClientCommandHandler : IRequestHandler<CreateClientCommand, Response<int>>
+    {
+        public async Task<Response<int>> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+        {
+            
+        }
+    }
+}
---
+++ BankAPI/Domain/Entities/Client.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Client : AuditableBaseEntity
+    {
+
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        
+        private int _age;
+        public int Age 
+        {
+            get
+            {
+                if (this._age <= 0)
+                {
+                    this._age = new DateTime(DateTime.Now.Subtract(this.BirthDate).Ticks).Year - 1;
+                }
+                return this._age;
+            }
+        }
+    }
+}
